4a003fda0f75ff00b19d178af11d1871c5699f5b
Emanuel Miranda <emanuelmiranda932@gmail.com>
2025-03-14 10:18:36 -0300

    Estilizando página index - Aula 07

--- to_do_lists/views.py
+++ to_do_lists/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from .forms import TarefaForm
 from .models import Tarefa
+# from django.http import HttpResponse
+from django.views.decorators.cache import never_cache
 
+@never_cache
 def index(request):
+
+    response = render(request, 'to_do_lists/index.html')
     
     if request.user.is_authenticated:
+        # O trecho de código a seguir configura os cabeçalhos HTTP na resposta
+        # para instruir o navegador a não armazenar a página em cache.
+        response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+        response['Pragma'] = 'no-cache'
+        response['Expires'] = '0'
+
         tarefas_total = Tarefa.objects.filter(user=request.user)
         if tarefas_total is not None:
             tarefas_concluidas = []
@@ -23,6 +34,6 @@ def index(request):
         context = {'form':form,
                    'tarefas_concluidas': tarefas_concluidas,
                    'tarefas_nao_concluidas': tarefas_nao_concluidas}
-        return render(request, 'to_do_lists/index.html', context)
-    else:
-        return render(request, 'to_to_lists/index.html')
+        response = render(request, 'to_do_lists/index.html', context)
+    
+    return response
